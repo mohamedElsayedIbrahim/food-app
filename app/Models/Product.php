6c2d440d92f6809cc3d70e34af505adf3219d5a4
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2024-12-23 12:13:24 +0200

    map category and product class together

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
@@ -14,5 +15,9 @@ class Product extends Model
 
     function orders() : BelongsToMany {
         return $this->belongsToMany(Order::class)->withPivot('quantity')->withTimestamps();
+    }
+
+    public function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
     }
 }
