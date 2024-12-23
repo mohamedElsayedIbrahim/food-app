12c9db6a1172014532a60cfd0bb09d461731dcc7
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2024-12-23 11:38:29 +0200

    insall api routing

---
+++ app/Http/Middleware/ApiAuthindiction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiAuthindiction
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::guard('api')->check()) {
+            // Modify this to redirect to a different route if necessary
+            return response()->json(['message' => 'Please login'], 401);
+        }
+        
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\ApiAuthindiction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware(ApiAuthindiction::class)->group(function(){
+    Route::resource('orders',OrderController::class);
+    Route::post('logout',[UserController::class,'logout']);
 });
+
+Route::resource('products',ProductController::class);
+Route::post('login',[UserController::class,'login']);
+Route::post('signup',[UserController::class,'signup']);
